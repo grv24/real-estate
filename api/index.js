e80b191b97c833b66fc6a4d24888a01b0bb4a471
gourav <gauravmahobe.mahobe@gmail.com>
2023-11-08 00:40:48 +0530

    setup mongodb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,20 @@
-import express from'express'
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('mongodb is connected')
+    }).catch((error) => {
+        console.log(error)
+    })
 
 const app = express()
 
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('server listening on port 3000')
 })
