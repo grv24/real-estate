859faaa7276f11a10d4e9344713bd759c5a1d170
gourav <gauravmahobe.mahobe@gmail.com>
2023-11-08 01:12:46 +0530

    setup-test-api

--- api/index.js
+++ api/index.js
@@ -1,20 +1,26 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 
-dotenv.config()
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log('mongodb is connected')
-    }).catch((error) => {
-        console.log(error)
-    })
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error);
+  });
 
-const app = express()
+const app = express();
 
+// Middleware to parse JSON requests
+app.use(express.json());
 
+// Using the userRouter
+app.use('/api/users', userRouter);
 
 app.listen(3000, () => {
-    console.log('server listening on port 3000')
-})
+  console.log('Server listening on port 3000');
+});
