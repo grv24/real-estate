7f6ffe462fbe222c3f32b4db213c1ec550d73915
gourav <gauravmahobe.mahobe@gmail.com>
2023-11-12 01:00:23 +0530

    sign-up-done-clientSide

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from 'cors'
 
 dotenv.config();
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
@@ -21,6 +23,7 @@ app.listen(3000, () => {
 });
 
 app.use(express.json());
+app.use(cors())
 
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter)
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   },
+//   plugins: [react()]
+// });
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // Use 'http://localhost:3000' instead of '::1:3000'
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  plugins: [react()]
+});
+
