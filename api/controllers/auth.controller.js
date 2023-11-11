6836772d9bb168db636b0d59756b13d5cab54943
gourav <gauravmahobe.mahobe@gmail.com>
2023-11-12 01:43:18 +0530

    sign-in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user-model.js";
 import bcryptjs from 'bcryptjs'
-export const signUp = async (req, res,next) => {
+import jwt from 'jsonwebtoken'
+export const signUp = async (req, res, next) => {
     try {
         const { username, email, password } = req.body;
 
@@ -19,3 +20,29 @@ export const signUp = async (req, res,next) => {
 }
 
 
+export const signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found !'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, 'Wrong credential!'))
+        }
+        // create token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        // remove pass-before sent it to the client
+        const {password: pass, ...rest} = validUser._doc
+        // Save the token in a cookie
+        console.log(rest)
+        res.cookie('access_token', token, {
+            httpOnly: true,
+        }).status(200).json({ rest });
+
+    } catch (error) {
+        next(error)
+    }
+}
